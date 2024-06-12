fb6c7bd5b5862fbccf65fa87ceb4a6dab256446d
Luca Azalim <lucaazalim@gmail.com>
2024-06-12 00:03:45 -0300

    fix: container class should not be used

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -15,9 +16,10 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
         <html lang="en">
         <body className={`{inter.className} bg-gray-100 text-gray-800`}>
         <Navbar/>
-        <div className="max-w-[1200px] px-10 mx-auto container">
+        <main className="max-w-[1200px] px-5 mx-auto">
             {children}
-        </div>
+        </main>
+        <Footer/>
         </body>
         </html>
     );
