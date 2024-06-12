77da8419e57182cc4603930be2387c22b6dcf244
Luca Azalim <lucaazalim@gmail.com>
2024-06-12 00:04:12 -0300

    feat: adding time-ago library

--- app/utils/datetime.ts
+++ app/utils/datetime.ts
@@ -1,24 +1,9 @@
-export function timeAgo(dateString: string): string {
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 
-    const date = new Date(dateString);
-    const now = new Date();
-    const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+TimeAgo.addDefaultLocale(en);
+const TIME_AGO = new TimeAgo("en-US");
 
-    const intervals = [
-        {label: 'ano', seconds: 31536000},
-        {label: 'mês', seconds: 2592000},
-        {label: 'dia', seconds: 86400},
-        {label: 'hora', seconds: 3600},
-        {label: 'minuto', seconds: 60},
-        {label: 'segundo', seconds: 1}
-    ];
-
-    for (const interval of intervals) {
-        const count = Math.floor(seconds / interval.seconds);
-        if (count >= 1) {
-            return `Há ${count} ${interval.label}${count > 1 ? 's' : ''}`;
-        }
-    }
-
-    return 'Agora mesmo';
+export function timeAgo(date: Date): string {
+    return TIME_AGO.format(date);
 }
